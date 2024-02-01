60c30956e0fd73cd3b44133cf7354c1b7afac7a3
Rolando Ramos Morales <rrmorales87@nauta.cu>
2024-01-31 23:21:00 -0500

    Add title search

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -5,31 +5,51 @@ const search = ref();
 </script>
 
 <template>
-<div class="w-full flex flex-row justify-between items-center gap-4 py-4 lg:px-24 md:px-14 sm:px-5">
-  <div class="flex flex-row flex-nowrap cursor-pointer">
-     <img class="icon-view" src="/storage/icon/subscripcion-icon.svg" alt="subscription logo">
-    <span class="menu-text px-2">{{$t("subscription")}}</span>
-  </div>
-  <div>
-    <img class="icon-logo" src="/storage/icon/rtravel-Logo.svg" alt="main logo">
-  </div>
-  <div class="flex flex-row gap-8 items-center">
-    <div class="menu-text flex flex-row gap-8 cursor-pointer">
-      <nav>{{$t('support')}}</nav>
-      <nav>{{$t('community')}}</nav>
-
+<div class="flex flex-col">
+  <div class="w-full flex flex-row justify-between items-center gap-4 py-4 lg:px-24 md:px-14 sm:px-5 mb-10">
+    <div class="flex flex-row flex-nowrap cursor-pointer">
+      <img class="icon-view" src="/storage/icon/subscripcion-icon.svg" alt="subscription logo">
+      <span class="menu-text px-2">{{$t("subscription")}}</span>
     </div>
-    <selector-language/>
-    <button class="btn-circuit flex flex-row justify-center items-center px-4 py-2 rounded-full">
-      <img class="btn-circuit_icon" src="/storage/icon/circuit-icon.svg" alt="circuit-icon">
-      <span class="btn-circuit_text">{{$t('circuits')}}</span>
-    </button>
-  </div>
+    <div>
+      <img class="icon-logo" src="/storage/icon/rtravel-Logo.svg" alt="main logo">
+    </div>
+    <div class="flex flex-row gap-8 items-center">
+      <div class="menu-text flex flex-row gap-8 cursor-pointer">
+        <nav>{{$t('support')}}</nav>
+        <nav>{{$t('community')}}</nav>
 
+      </div>
+      <selector-language/>
+      <button class="btn-circuit flex flex-row justify-center items-center px-4 py-2 rounded-full">
+        <img class="btn-circuit_icon" src="/storage/icon/circuit-icon.svg" alt="circuit-icon">
+        <span class="btn-circuit_text">{{$t('circuits')}}</span>
+      </button>
+    </div>
+
+  </div>
+  <div class="flex flex-row justify-center items-center">
+    <h1 class="title-search">{{$t("title-search1")}} <span class="title-cuba"> Cuba </span>{{$t("title-search2")}}</h1>
+  </div>
 </div>
+
+
 </template>
 
 <style  lang="scss">
+.title-search {
+  font-size: 40px;
+  font-weight: 600;
+  line-height: 55px;
+  letter-spacing: 0;
+  text-align: left;
+  color: #0F3B53;
+  .title-cuba {
+    color:#0084BD;
+  }
+
+
+}
 .icon-view {
   width: 27px;
   height: 18.81px;
