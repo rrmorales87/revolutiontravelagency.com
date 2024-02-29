68f93d16e99c5c912c7616a6479a08bd121070bb
Rolando Ramos Morales <rrmorales87@nauta.cu>
2024-02-28 23:36:20 -0500

    Add form to join or login

---
+++ resources/js/Components/TermsUse.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="terms">
+    <p>
+      <span>{{$t('terms1')}}</span>
+      <span><a href="#" class="text-blue-500 underline">{{$t('terms2')}}</a></span>
+      <span>{{$t('terms3')}}</span>
+      <span><a href="#" class="text-blue-500 underline">{{$t('terms4')}}</a></span>
+    </p>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+.terms {
+  font-size: 0.6rem;
+  @apply font-Lato
+}
+</style>
