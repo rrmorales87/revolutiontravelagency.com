dae8c8e1100062b474a3d91d385057051bab202f
Rolando Ramos Morales <rrmorales87@nauta.cu>
2024-03-04 08:49:19 -0500

    Add chat

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,22 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body>
         @inertia
+
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/65e5ce3b9131ed19d974c793/1ho4qr6tt';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+        <!--End of Tawk.to Script-->
+
     </body>
 </html>
